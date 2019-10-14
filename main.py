d35970d3cd5ad9bae0c71e407dea3045564becc4
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-10-14 11:48:34 -0400

    switching to pygame

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-from direct.showbase.ShowBase import ShowBase
-from direct.actor.Actor import Actor
- 
-class MyApp(ShowBase):
- 
-    def __init__(self):
-        ShowBase.__init__(self)
+import pygame
 
-        self.pandaActor = Actor("models/panda-model",
-                                {"walk": "models/panda-walk4"})
-        self.pandaActor.setScale(0.005, 0.005, 0.005)
-        self.pandaActor.reparentTo(self.render)
-        self.pandaActor.loop("walk")
-
-        self.camera.setPos(20, -30, 3)
- 
-base = MyApp()
-base.run()
