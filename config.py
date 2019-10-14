746c1c846e7fc061a7dbaabd49a1fda798676b1b
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-10-14 12:01:27 -0400

    setup pygame

---
+++ config.py
@@ -0,0 +1,9 @@
+from contants import *
+
+background_color = WHITE
+body_1_color = RED
+body_2_color = GREEN
+body_3_color = BLUE
+
+width = 800
+height = 600
--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import pygame
+from config import *
+import sys
 
+pygame.init()
+
+surface = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Three Body Problem')
+
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(10)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    surface.fill(background_color)
+    pygame.draw.circle(surface, body_1_color, [200, 200], 10)
+
+    pygame.display.flip()
