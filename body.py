7930655a03aa1ef110cc73391eb548d5958e8588
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-12-07 00:51:43 -0500

    changing numbers

--- body.py
+++ body.py
@@ -5,6 +5,9 @@ from contants import G
 
 class Body:
 
+    min_r = 100
+    num_updates = 10
+
     def __init__(self, m, r, postion, velocity, color=body_1_color, movable=True):
         self.m = m
         self.r = r
@@ -18,14 +21,24 @@ class Body:
         
         for body in bodies:
             if body != self:
-                mag_a = G * self.m * body.m / sum(np.square(np.subtract(self.position, body.position)))
+                r_squared = sum(np.square(np.subtract(self.position, body.position)))
+                if r_squared < Body.min_r:
+                    r_squared = Body.min_r
+                mag_a = G *body.m / r_squared
                 self.a = np.add(self.a, np.multiply(np.subtract(body.position, self.position), mag_a / np.linalg.norm(np.subtract(body.position, self.position))))
+            for body2 in bodies:
+                diff = np.subtract(body.position, body2.position)
+                dist = np.linalg.norm(diff)
+                if body != body2 and dist < 10:
+                    body.velocity = -np.array(body.velocity)
+                    body2.velocity = -np.array(body2.velocity)
 
 
     def update(self, dt):
-        if self.movable:
-            self.velocity = np.add(self.velocity, np.multiply(self.a, dt))
-            self.position = np.add(self.position, np.multiply(self.velocity, dt))
+        for _ in range(Body.num_updates):
+            if self.movable:
+                self.velocity = np.add(self.velocity, np.multiply(self.a, dt / Body.num_updates))
+                self.position = np.add(self.position, np.multiply(self.velocity, dt / Body.num_updates))
 
     def draw(self, surface):
         pygame.draw.circle(surface, self.color, [int(x) for x in self.position], int(self.r))
--- main.py
+++ main.py
@@ -12,8 +12,9 @@ pygame.display.set_caption('Three Body Problem')
 
 clock = pygame.time.Clock()
 
-# bodies = [Body(10, 10, [300, 100], [0, 200], body_1_color), Body(100, 10, [500, 100], [0, 0], body_2_color)]
-bodies = [Body(10, 10, [100, 500], [0, 0], body_1_color), Body(10, 10, [500, 500], [0, 0], body_2_color), Body(10, 10, [300, 100], [0, 0], body_3_color)]
+bodies = [Body(10, 10, [300, 100], [0, 300], body_1_color), Body(10, 10, [500, 100], [-100, 0], body_2_color)]
+# bodies = [Body(10, 10, [100, 500], [0, 0], body_1_color), Body(10, 10, [500, 500], [0, 0], body_2_color), Body(10, 10, [300, 100], [0, 0], body_3_color)]
+# bodies = [Body(100, 30, [400, 300], [0, 0], body_1_color, False), Body(10, 10, [400, 50], [600, 0], body_2_color)]
 
 current_time = time.time()
 
